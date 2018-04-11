60de05da54895bdcc14bea9ac091a16cc5eaf5f7
Travis <Travisagrossman@gmail.com>
2018-04-11 14:33:17 -0700

    change wiki policy and index scope

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,6 +1,37 @@
 class WikiPolicy < ApplicationPolicy
   attr_reader :user, :wiki
 
+  class Scope
+       attr_reader :user, :scope
+
+       def initialize(user, scope)
+         @user = user
+         @scope = scope
+       end
+
+       def resolve
+         wikis = []
+         if user.role == 'admin'
+           wikis = scope.all # if the user is an admin, show them all the wikis
+         elsif user.role == 'premium'
+           all_wikis = scope.all
+           all_wikis.each do |wiki|
+             if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+               wikis << wiki 
+             end
+           end
+         else # this is the lowly standard user
+           all_wikis = scope.all
+           wikis = []
+           all_wikis.each do |wiki|
+             if wiki.public? || wiki.collaborators.include?(user)
+               wikis << wiki
+             end
+           end
+         end
+         wikis
+       end
+     end
   def initialize(user, wiki)
     @user = user
     @wiki = wiki

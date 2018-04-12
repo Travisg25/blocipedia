f03bbd05ac16b451d6c752a4b9cadb76baed2469
Travis <Travisagrossman@gmail.com>
2018-04-12 11:04:04 -0700

    added downgrade controller

---
+++ app/controllers/downgrades_controller.rb
@@ -0,0 +1,9 @@
+class DowngradesController < ApplicationController
+
+  def update
+    current_user.update! role: 0
+    redirect_to wikis_path
+    flash[:notice] = "You have been downgraded to a free account, all private wikis are now public."
+  end
+
+end

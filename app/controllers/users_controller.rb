f02d55b44f63e3b97b47413d20cc9a251f9ffd24
Travis <Travisagrossman@gmail.com>
2018-04-16 15:15:19 -0700

    user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def downgrade
-    @user = current_user
-    current_user.update_attribute(:role, 'standard')
+      @user = current_user
+      wikis = user.posts.where(private: true)
 
-    flash[:notice] = "Your account has been downgraded."
-    redirect_to users_show_path
-  end
+      current_user.update_attribute(:role, 'standard')
+      wikis.each do |wiki|
+        if wiki.user.id == current_user.id
+          wiki.update_attribute(:private, false)
+        end
+      end
+      flash[:notice] = "Your account has been downgraded. You are now a Standard member."
+      redirect_to users_show_path
+    end
 end

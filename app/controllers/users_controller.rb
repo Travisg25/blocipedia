9e7e296bc1c27291089061c00cf07c2f3d88b824
Travis <Travisagrossman@gmail.com>
2018-04-17 18:53:58 -0700

    adding correct association between models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
   def downgrade
       @user = current_user
-      wikis = user.posts.where(private: true)
+      wikis = current_user.wikis.where(private: true)
 
       current_user.update_attribute(:role, 'standard')
       wikis.each do |wiki|
         if wiki.user.id == current_user.id
-          wiki.update_attribute(:private, false)
+          wikis.update_all(public: true)
         end
       end
       flash[:notice] = "Your account has been downgraded. You are now a Standard member."

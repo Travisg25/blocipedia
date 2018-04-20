35caec31f935b4ef6acb2345d529be8387edebd9
Travis <Travisagrossman@gmail.com>
2018-04-20 12:34:15 -0700

    deleting unnessary loop in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,9 @@ class UsersController < ApplicationController
       wikis = current_user.wikis.where(private: true)
 
       current_user.update_attribute(:role, 'standard')
-      wikis.each do |wiki|
-        if wiki.user.id == current_user.id
-          update_all(private: false)
-        end
-      end
+      wikis.update_all(private: false)
+
+
       flash[:notice] = "Your account has been downgraded. You are now a Standard member."
       redirect_to users_show_path
     end

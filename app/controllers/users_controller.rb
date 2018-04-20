df16af8027fb0f917fc57bc5e8438792e5f69e74
Travis <Travisagrossman@gmail.com>
2018-04-19 17:43:55 -0700

    wiki collab correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,17 @@ class UsersController < ApplicationController
 
   def downgrade
       @user = current_user
-      wikis = user.wikis.where(private: true)
+      wikis = current_user.wikis.where(private: true)
 
       current_user.update_attribute(:role, 'standard')
       wikis.each do |wiki|
         if wiki.user.id == current_user.id
-          wiki.update_attribute(:private, false)
+          update_all(private: false)
         end
       end
       flash[:notice] = "Your account has been downgraded. You are now a Standard member."
       redirect_to users_show_path
     end
+  end
+
 end

ae9f75289a205fcb34b2ad3248bfc8a321fe8e36
Travis <Travisagrossman@gmail.com>
2018-04-19 17:47:18 -0700

    deleting redundantscope

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -4,7 +4,6 @@ class Wiki < ApplicationRecord
   has_many :collaborators
   has_many :users, through: :collaborators
 
-  default_scope { order('created_at DESC') }
 
   def initialize_role
     self.private = false if self.private.nil?
